6aed31494cbe7bdd52bd6e740a898293d5f55466
Morgan Roff <roffmorgan@gmail.com>
2016-11-22 15:00:39 -0500

    change matrix_image to use ndarray

--- data/matrix_image.py
+++ data/matrix_image.py
@@ -1,12 +1,10 @@
 # Generate a 2D-matrix image
 
 from math import sin, cos
-from numpy import matrix
+from numpy import array, zeros, int8
 
-from data.cartesian import Point, get_perp_line_through_point
-
-def _generate_2d_matrix(width=5, height=5, init_val=0):
-	return matrix([[init_val for x in range(width)] for y in range(height)])
+from data.cartesian import Point
+from data.shapes import get_perp_line_through_point
 
 def is_part_of_line(x, y, w, h, line):
 	p = Point(x - (w - 1)/2, (h-1)/2 - y)
@@ -19,10 +17,10 @@ def is_part_of_line(x, y, w, h, line):
 	return p.calc_distance(intercept) <= 0.5
 
 def get_line_image(line, w=5, h=5):
-	m = _generate_2d_matrix(w, h, 0)
+	m = zeros((h, w), int8)
 
 	for x in range(0, w):
 		for y in range(0, h):
 			if is_part_of_line(x, y, w, h, line):
-				m.A[y][x] = 1
+				m[y, x] = 1
 	return m
