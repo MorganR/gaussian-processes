a9e361de1ec74758a06552e67834bea449bdfaac
Morgan Roff <roffmorgan@gmail.com>
2017-02-06 13:58:55 -0500

    time optimizations and add constant mean

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from mnist import MNIST
 import numpy as np
 import matplotlib.pyplot as plt
 import GPflow
+import time
 
 mnist = MNIST()
 
@@ -17,7 +18,7 @@ print(mnist)
 
 # Setup GP data
 num_digits = 10
-num_images_per_digit = 25
+num_images_per_digit = 11
 X = np.zeros((num_digits * num_images_per_digit, mnist.train_rows * mnist.train_cols), dtype=np.float64)
 Y = np.zeros((num_digits * num_images_per_digit), dtype=np.int32)
 for i in range(0, num_images_per_digit):
@@ -25,21 +26,26 @@ for i in range(0, num_images_per_digit):
         Y[i + i*y] = y
         X[i + i*y] = mnist.train_ordered[y][i, :, :].flatten().astype(np.float64) / 255
 
-print(Y.size, Y.shape)
-print(X.size, X.shape)
+constant_mean = GPflow.mean_functions.Constant(c=0.1)
+constant_mean.fixed = False
 
 m = GPflow.vgp.VGP(
     X,
     Y,
-    kern=GPflow.kernels.RBF(mnist.train_rows * mnist.train_cols),
+    kern=GPflow.kernels.RBF(input_dim=(mnist.train_rows * mnist.train_cols)),
     likelihood=GPflow.likelihoods.MultiClass(num_digits),
+    mean_function=constant_mean,
     num_latent=num_digits)
 
-m.optimize(maxiter=10000)
+t_start = time.time()
 
-print('Optimization complete.')
+m.optimize(maxiter=5000)
 
-num_test = 5
+t_end = time.time()
+
+print('Optimization completed in {} seconds'.format(t_end - t_start))
+
+num_test = 6
 X_test = mnist.test_images[0:num_test, :, :].reshape((num_test), mnist.test_rows*mnist.test_cols) \
     .astype(np.float64) / 255
 Y_test = mnist.test_labels[0:num_test].astype(np.int32)
@@ -50,5 +56,5 @@ p, _ = m.predict_y(X_test)
 for i in range(0, num_test):
     print("Image {}, Digit {}".format(i, Y_test[i]))
     for c in range(m.likelihood.num_classes):
-        print('\tmean_{}: {}, variance_{}: {}'.format(c, mu[i,c], c, var[i,c]))
+        # print('\tmean_{}: {}, variance_{}: {}'.format(c, mu[i,c], c, var[i,c]))
         print('\tprobability_{}: {}'.format(c, p[i,c]))
