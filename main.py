2e17f32a9843730e9fe72fad4583ad67914ad75a
MorganR <roffmorgan@gmail.com>
2017-03-21 12:40:12 -0400

    Enable different kernels

--- main.py
+++ main.py
@@ -8,18 +8,22 @@ from vgp_tester import VgpTester
 from mcmc_tester import McmcTester
 
 num_per_digits = [50]
-num_inducing_inputs = [20, 50, 100, 150, 250, 500]
+# num_inducing_inputs = [20, 50, 100, 150, 250, 500]
 
 for num_per_digit in num_per_digits:
 
-    digits = np.arange(0, 10)
+    digits = np.array([0, 1, 2])
     data = get_mnist_data(digits, num_per_digit)
-    data = data.get_pca_data(75)
+    data = data.get_pca_data(2)
 
-    for num_inducing_input in num_inducing_inputs:
-        m_test = SvgpTester(data, num_inducing_input)
-        m_test.train()
-        m_test.test(10000)
+    m_test = VgpTester(data, 'm12')
+    m_test.train()
+    m_test.test(10000)
+
+    # for num_inducing_input in num_inducing_inputs:
+    #     m_test = SvgpTester(data, num_inducing_input)
+    #     m_test.train()
+    #     m_test.test(10000)
 # from mpl_toolkits.mplot3d import Axes3D
 # import matplotlib.cm as cm
 # fig = plt.figure()
@@ -34,36 +38,36 @@ for num_per_digit in num_per_digits:
 # m_test.plot_z(axes, num_rows, num_cols)
 
 
-# num_rows = 2
-# num_cols = 5
+    num_cols = min([len(digits), 5])
+    num_rows = int(np.ceil(len(digits)/num_cols))
 
-# fig, axes = plt.subplots(num_rows, num_cols)
-# if num_rows == 1 and num_cols == 1:
-#     data.plot_pca_data(axes, num_per_digit)
-#     m_test.visualize_density(axes, 0)
-#     axes.legend()
-# elif num_rows > 1:
-#     for r in np.arange(0, num_rows):
-#         for col in np.arange(0, num_cols):
-#             data.plot_pca_data(axes[r,col], 15)
-#             m_test.visualize_density(axes[r,col], r*num_cols + col)
-#             axes[r,col].set_title('{}'.format(digits[r*num_cols + col]))
-# else:
-#     for col in np.arange(0, num_cols):
-#         data.plot_pca_data(axes[col], 15)
-#         m_test.visualize_density(axes[col], col)
-#         axes[col].set_title('{}'.format(digits[col]))
+    fig, axes = plt.subplots(num_rows, num_cols)
+    if num_rows == 1 and num_cols == 1:
+        data.plot_pca_data(axes, num_per_digit)
+        m_test.visualize_density(axes, 0)
+        axes.legend()
+    elif num_rows > 1:
+        for r in np.arange(0, num_rows):
+            for col in np.arange(0, num_cols):
+                data.plot_pca_data(axes[r,col], 15)
+                m_test.visualize_density(axes[r,col], r*num_cols + col)
+                axes[r,col].set_title('{}'.format(digits[r*num_cols + col]))
+    else:
+        for col in np.arange(0, num_cols):
+            data.plot_pca_data(axes[col], 15)
+            m_test.visualize_density(axes[col], col)
+            axes[col].set_title('{}'.format(digits[col]))
 
-# fig.suptitle('GP Classification Using {} Images and {} Inducing Inputs'.format(
-#     data.y.size, m_test.inducing_inputs.shape[0]
-# ))
-# fig.set_size_inches(18, 8)
-# fig.subplots_adjust(top=0.91, bottom=0.05)
-# fig.savefig('{}-{}.png'.format(data.y.size, m_test.inducing_inputs.shape[0]))
-# fig.suptitle('GP Classification on 2D Data\nUsing {} Images with VGP'.format(
-#     data.y.size
-# ))
-# fig.set_size_inches(16, 8)
-# fig.subplots_adjust(top=0.89, bottom=0.05)
-# fig.savefig('vgp-{}c-{}.png'.format(len(digits), data.y.size))
-# plt.show()
+    # fig.suptitle('GP Classification Using {} Images and {} Inducing Inputs'.format(
+    #     data.y.size, m_test.inducing_inputs.shape[0]
+    # ))
+    # fig.set_size_inches(18, 8)
+    # fig.subplots_adjust(top=0.91, bottom=0.05)
+    # fig.savefig('{}-{}.png'.format(data.y.size, m_test.inducing_inputs.shape[0]))
+    fig.suptitle('GP Classification on 2D Data\nUsing {} Images with VGP'.format(
+        data.y.size
+    ))
+    # fig.set_size_inches(16, 8)
+    # fig.subplots_adjust(top=0.89, bottom=0.05)
+    fig.savefig('vgp-{}c-{}.png'.format(len(digits), data.y.size))
+    plt.show()
--- vgp_tester.py
+++ vgp_tester.py
@@ -3,13 +3,24 @@ import GPflow
 from model_tester import ModelTester
 
 class VgpTester(ModelTester):
-    def __init__(self, data):
+    def __init__(self, data, kern_id):
         self.num_classes = np.unique(data.y).size
         self.num_dimensions = data.x.shape[1]
+        self.kern_id = kern_id
+
+        if kern_id == 'rbf':
+            kernel = GPflow.kernels.RBF(input_dim=self.num_dimensions, ARD=True, lengthscales=0.1*np.ones(self.num_dimensions))
+        elif kern_id == 'm12':
+            kernel = GPflow.kernels.Matern12(input_dim=self.num_dimensions, ARD=True, lengthscales=0.1*np.ones(self.num_dimensions))
+        elif kern_id == 'm32':
+            kernel = GPflow.kernels.Matern32(input_dim=self.num_dimensions, ARD=True, lengthscales=0.1*np.ones(self.num_dimensions))
+        elif kern_id == 'm52':
+            kernel = GPflow.kernels.Matern52(input_dim=self.num_dimensions, ARD=True, lengthscales=0.1*np.ones(self.num_dimensions))
+
         model = GPflow.vgp.VGP(
             data.x,
             data.y,
-            kern=GPflow.kernels.RBF(input_dim=self.num_dimensions),
+            kern=kernel,
             likelihood=GPflow.likelihoods.MultiClass(self.num_classes),
             num_latent=self.num_classes)
         super().__init__(data, model)
@@ -17,6 +28,6 @@ class VgpTester(ModelTester):
     def train(self):
         print('Training against {} classes using {} images.'.format(self.num_classes, self.data.y.size))
         self.optimize()
-        self.export('vgp-{}c-{}d-{}i-nopca'.format(
+        self.export('vgp-{}c-{}d-{}i-'.format(
             self.num_classes, self.num_dimensions, self.data.y.size
-        ))
+        ) + self.kern_id)
