fea5ad64f515e60bbb789639d46d8718a5790a04
Morgan Roff <roffmorgan@gmail.com>
2017-03-23 19:40:21 -0400

    Prepare galaxy-main for testing

---
+++ galaxy-main.py
@@ -0,0 +1,67 @@
+# Galaxy main test file
+import numpy as np
+import matplotlib.pyplot as plt
+from model_tester import ModelTester, import_model_tester
+from data_holder import DataHolder, get_mnist_data, get_galaxy_data
+from svgp_tester import SvgpTester
+from vgp_tester import VgpTester
+from mcmc_tester import McmcTester
+
+data = get_galaxy_data(250)
+
+m_test = VgpTester(data, 'rbf')
+m_test.train()
+m_test.test(2000)
+
+# for num_inducing_input in num_inducing_inputs:
+#     m_test = SvgpTester(data, 'm32', num_inducing_input)
+#     m_test.train()
+#     m_test.test(10000)
+
+# from mpl_toolkits.mplot3d import Axes3D
+# import matplotlib.cm as cm
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# colors = cm.rainbow(np.linspace(0, 1, num_digits))
+
+# for i,c in zip(np.unique(Y), colors):
+#     ax.plot(X[Y==i,0], X[Y==i,1], zs=X[Y==i,2], linestyle='', marker='o', label=i, c=c)
+
+# num_inducing=100
+# m_test = SvgpTester(data, num_inducing)
+# m_test.plot_z(axes, num_rows, num_cols)
+
+
+    # num_cols = min([len(digits), 5])
+    # num_rows = int(np.ceil(len(digits)/num_cols))
+
+    # fig, axes = plt.subplots(num_rows, num_cols)
+    # if num_rows == 1 and num_cols == 1:
+    #     data.plot_pca_data(axes, num_per_digit)
+    #     m_test.visualize_density(axes, 0)
+    #     axes.legend()
+    # elif num_rows > 1:
+    #     for r in np.arange(0, num_rows):
+    #         for col in np.arange(0, num_cols):
+    #             data.plot_pca_data(axes[r,col], 15)
+    #             m_test.visualize_density(axes[r,col], r*num_cols + col)
+    #             axes[r,col].set_title('{}'.format(digits[r*num_cols + col]))
+    # else:
+    #     for col in np.arange(0, num_cols):
+    #         data.plot_pca_data(axes[col], 15)
+    #         m_test.visualize_density(axes[col], col)
+    #         axes[col].set_title('{}'.format(digits[col]))
+
+    # fig.suptitle('GP Classification Using {} Images and {} Inducing Inputs'.format(
+    #     data.y.size, m_test.inducing_inputs.shape[0]
+    # ))
+    # fig.set_size_inches(18, 8)
+    # fig.subplots_adjust(top=0.91, bottom=0.05)
+    # fig.savefig('{}-{}.png'.format(data.y.size, m_test.inducing_inputs.shape[0]))
+    # fig.suptitle('GP Classification on 2D Data\nUsing {} Images with VGP'.format(
+    #     data.y.size
+    # ))
+    # fig.set_size_inches(7, 4)
+    # fig.subplots_adjust(top=0.8, bottom=0.05)
+    # fig.savefig('vgp-{}c-{}.png'.format(len(digits), data.y.size))
+    # plt.show()
