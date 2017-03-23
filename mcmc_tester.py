eb11630616ebd24a86977438899f28c0b2d9f2bf
Morgan Roff <roffmorgan@gmail.com>
2017-03-23 14:02:31 -0400

    Enable multiple kernels for mcmc and svgp

--- mcmc_tester.py
+++ mcmc_tester.py
@@ -3,12 +3,23 @@ import GPflow
 from model_tester import ModelTester
 
 class McmcTester(ModelTester):
-    def __init__(self, data, num_inducing):
+    def __init__(self, data, kern_id, num_inducing):
         self.num_classes = np.unique(data.y).size
         self.num_dimensions = data.x.shape[1]
 
         self.inducing_offset = int(round(data.y.size / num_inducing))
         self.inducing_inputs = data.x[::self.inducing_offset].copy()
+
+        self.kern_id = kern_id
+
+        if kern_id == 'rbf':
+            kernel = GPflow.kernels.RBF(input_dim=self.num_dimensions, ARD=True)
+        elif kern_id == 'm12':
+            kernel = GPflow.kernels.Matern12(input_dim=self.num_dimensions, ARD=True)
+        elif kern_id == 'm32':
+            kernel = GPflow.kernels.Matern32(input_dim=self.num_dimensions, ARD=True)
+        elif kern_id == 'm52':
+            kernel = GPflow.kernels.Matern52(input_dim=self.num_dimensions, ARD=True)
 
         print('Creating SGPMC model with {} classes, {} dimensions, {} images and {} inducing inputs'
             .format(self.num_classes, self.num_dimensions,
@@ -16,7 +27,7 @@ class McmcTester(ModelTester):
         model = GPflow.sgpmc.SGPMC(
             data.x,
             data.y,
-            kern=GPflow.kernels.RBF(input_dim=self.num_dimensions),
+            kern=kernel,
             likelihood=GPflow.likelihoods.MultiClass(self.num_classes),
             Z=self.inducing_inputs,
             num_latent=self.num_classes)
@@ -26,6 +37,6 @@ class McmcTester(ModelTester):
     def train(self):
         print('Training against {} classes using {} images.'.format(self.num_classes, self.data.y.size))
         self.optimize()
-        self.export('mcmc-{}c-{}d-{}i-{}z'.format(
+        self.export('mcmc-{}c-{}d-{}i-{}z-'.format(
             self.num_classes, self.num_dimensions, self.data.y.size, self.inducing_inputs.shape[0]
-        ))
+        ) + self.kern_id + '-gpu')
