1efda8c79cf26368ee64673b96f135a0aa571b56
Morgan Roff <roffmorgan@gmail.com>
2017-03-24 12:27:00 -0400

    Download larger data set, 10000 images per class

--- galaxies/image-collector.py
+++ galaxies/image-collector.py
@@ -5,13 +5,13 @@ import base64
 from PIL import Image
 import io
 
-num_images = 2000
+num_images = 10000
 
 img_size = 64
 scale = 0.05
 url = 'http://casjobs.sdss.org/ImgCutoutDR7/ImgCutout.asmx/GetJpeg'
-base_files = ['spirals', 'ellipticals', 'uncertains', 'spiral-edges']
-files = ('2000-'+bf for bf in base_files)
+base_files = ['spirals', 'ellipticals', 'uncertains', 'spiral-edges', 'uncertain-edges']
+files = ('10000-'+bf for bf in base_files)
 
 for bf,f in zip(base_files, files):
     objid, spiral, elliptical, uncertain, nvote = np.loadtxt(
@@ -26,10 +26,14 @@ for bf,f in zip(base_files, files):
     byte_filenames = [
         str(train_images)+'-train-'+bf+'.ubyte',
         str(test_images)+'-test-'+bf+'.ubyte']
+    id_filenames = [
+        str(train_images)+'-train-ids-'+bf+'.ubyte',
+        str(test_images)+'-test-ids-'+bf+'.ubyte'
+    ]
     folders = ['train-'+bf, 'test-'+bf]
     
     start_i = 0
-    for byte_filename, num_per_file, folder in zip(byte_filenames, [train_images, test_images], folders):
+    for byte_filename, id_filename, num_per_file, folder in zip(byte_filenames, id_filenames, [train_images, test_images], folders):
         with open(byte_filename, 'wb') as image_byte_data:
             # print number of images
             image_byte_data.write((num_per_file).to_bytes(4, byteorder='big'))
@@ -38,31 +42,38 @@ for bf,f in zip(base_files, files):
             # print num image cols
             image_byte_data.write((img_size).to_bytes(4, byteorder='big'))
 
-            for i in range(start_i, num_per_file + start_i):
-                payload = {
-                    'ra_': ra[i],
-                    'dec_': dec[i],
-                    'scale_': scale*petroRad_i[i],
-                    'width_': img_size,
-                    'height_': img_size,
-                    'opt_': ''# ,
-                    # 'query_': '',
-                    # 'imgtype_': '',
-                    # 'imgfield_': ''
-                }
-                result = requests.get(url, params=payload)
-                parser = re.compile(r'<base64Binary xmlns="[^"]+">([^<]+)</base64Binary>')
-                parsed_result = parser.search(str(result.content))
-                encoded_img = parsed_result.group(1)
-                img = base64.b64decode(encoded_img)
-                img_base_name = folder+'/{}'.format(objid[i])
-                img_name = img_base_name+'.jpg'
-                with open(img_name, 'wb') as img_file:
-                    img_file.write(img)
-                im = Image.open(img_name)
-                # convert to black and white
-                im_bw = im.convert('L')
-                im_bw.save(img_base_name+'-bw.jpg')
-                np_array = np.array(im_bw.getdata(), dtype=np.uint8)
-                image_byte_data.write(np_array.tobytes())
+            with open(id_filename, 'wb') as id_file:
+                id_file.write((num_per_file).to_bytes(4, byteorder='big'))
+
+                for i in range(start_i, num_per_file + start_i):
+                    payload = {
+                        'ra_': ra[i],
+                        'dec_': dec[i],
+                        'scale_': scale*petroRad_i[i],
+                        'width_': img_size,
+                        'height_': img_size,
+                        'opt_': ''# ,
+                        # 'query_': '',
+                        # 'imgtype_': '',
+                        # 'imgfield_': ''
+                    }
+                    # write objid to file
+                    id_file.write(int(objid[i]).to_bytes(8, byteorder='big'))
+                    # get image
+                    result = requests.get(url, params=payload)
+                    parser = re.compile(r'<base64Binary xmlns="[^"]+">([^<]+)</base64Binary>')
+                    parsed_result = parser.search(str(result.content))
+                    encoded_img = parsed_result.group(1)
+                    img = base64.b64decode(encoded_img)
+                    save image to file
+                    img_base_name = folder+'/{}'.format(objid[i])
+                    img_name = img_base_name+'.jpg'
+                    with open(img_name, 'wb') as img_file:
+                        img_file.write(img)
+                    im = Image.open(img_name)
+                    convert to black and white
+                    im_bw = im.convert('L')
+                    im_bw.save(img_base_name+'-bw.jpg')
+                    np_array = np.array(im_bw.getdata(), dtype=np.uint8)
+                    image_byte_data.write(np_array.tobytes())
             start_i = num_per_file
